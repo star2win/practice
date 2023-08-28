efa1af71d5dda43ac51989d4c23d4270fb2cfff5
Filip Szymanski <fszymanski@gmail.com>
2023-08-28 12:02:19 -0700

    Initial main function

---
+++ bagels.py
@@ -0,0 +1,14 @@
+"""Bagels game by Filip Szymanski
+Practice coding game"""
+
+MAX_DIGITS = 3
+
+def main():
+    """Main loop of program"""
+
+    print(f'This is a guess a {MAX_DIGITS} digit game')
+
+    while(False):
+        print('test')
+if __name__ == '__main__':
+    main()
